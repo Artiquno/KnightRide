a6b9e6ec3e5fdcf673bffe5c849034ce60410d65
CodeRend <en31995@gmail.com>
2014-08-14 14:02:06 +0200

    Having and idea of how to make this work
    I'm getting the idea to making this work, and have started it a bit. However it's still in it's begginings. And i still don't know why i'm writing notes to myself about the past...

--- Board.h
+++ Board.h
@@ -7,13 +7,13 @@ class Square
 public:
     Square();
 
-    const bool getTaken() const
+    const bool getAvail() const
     {
-        return taken;
+        return avail;
     }
-    void setTaken(bool val)
+    void setAvail(bool val)
     {
-        taken = val;
+        avail = val;
     }
 
     const int getPriority() const
@@ -22,18 +22,30 @@ public:
     }
     void setPriority(int val);  //Note: Check if it's between 0 and 7
 private:
-    bool taken;
+    bool avail;
     int priority;   //0-7; low priority to high
 };  //Square class
 
 class Board
 {
 public:
-    Board();
+    Board();    //Start in the center (somewhere around there...)
+    Board(int startRow, int startCol = 0);  //Different starting place
 
-    virtual void print() const;
+    //The '&' should make it cascadeable. If not, FUCK IT!!!
+    const Square& operator[](int index) const;   //Check for out_of_range
+    Square& operator[](int index);   //Same, just non-const
+
+    virtual void setPriorities();   //Set priorities of the movable-to squares
+
+    virtual void move(int row, int col);    //This will change the current position
+    virtual bool checkMoves();  //This will modify the availabilities of the squares
+
+    virtual void print() const; //Print the whole board
 private:
-    Square square[8][8];
+    int currRow;
+    int currCol;
+    Square squares[8][8];
 };  //Board class
 
 #endif // BOARD_H
